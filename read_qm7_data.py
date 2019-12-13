941fd81c8156266afeab044de598b7f748c07d57
David Luevano <55825613+luevano@users.noreply.github.com>
2019-12-12 19:39:43 -0700

    Fix spelling

--- read_qm7_data.py
+++ read_qm7_data.py
@@ -25,10 +25,10 @@ import time
 import numpy as np
 import random
 from misc import printc
+
+
 # 'periodic_table_of_elements.txt' retrieved from
 # https://gist.github.com/GoodmanSciences/c2dd862cd38f21b0ad36b8f96b4bf1ee
-
-
 def read_nc_data(data_path):
     """
     Reads nuclear charge data from file and returns a dictionary.
