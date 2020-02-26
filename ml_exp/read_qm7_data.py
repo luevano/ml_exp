339954ffc95c29327d33e3e2697fea1cf8e9427d
David Luevano Alvarado <55825613+luevano@users.noreply.github.com>
2020-02-25 21:03:31 -0700

    Remove unused functions

--- ml_exp/read_qm7_data.py
+++ ml_exp/read_qm7_data.py
@@ -25,39 +25,6 @@ import numpy as np
 import random
 
 
-# 'periodic_table_of_elements.txt' retrieved from
-# https://gist.github.com/GoodmanSciences/c2dd862cd38f21b0ad36b8f96b4bf1ee
-def read_nc_data(data_path):
-    """
-    Reads nuclear charge data from file and returns a dictionary.
-    data_path: path to the data directory.
-    """
-    fname = 'periodic_table_of_elements.txt'
-    with open(''.join([data_path, '/', fname]), 'r') as infile:
-        temp_lines = infile.readlines()
-
-    del temp_lines[0]
-
-    lines = []
-    for temp_line in temp_lines:
-        new_line = temp_line.split(sep=',')
-        lines.append(new_line)
-
-    # Dictionary of nuclear charge.
-    return {line[2]: int(line[0]) for line in lines}
-
-
-# For use in the rewriting of the code (from functional programming to object
-# oriented programming-ish)
-def print_nc(nc_data):
-    """
-    Prints the nuclear charge data to the terminal.
-    nc_data: dict of nc.
-    """
-    for key in nc_data.keys():
-        print(f'\'{key}\': {nc_data[key]},')
-
-
 # 'hof_qm7.txt.txt' retrieved from
 # https://github.com/qmlcode/tutorial
 def read_db_data(zi_data,
